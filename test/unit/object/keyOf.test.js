677403f3c00f3375c770e3373ca7fa99d078aec1
Sascha Goldhofer <post@saschagoldhofer.de>
2014-10-27 18:33:14 +0100

    Add tests for object helper

---
+++ test/unit/object/keyOf.test.js
@@ -0,0 +1,32 @@
+var expect = require("chai").expect;
+
+var o = require("../../../lib/object");
+
+
+describe("object", function () {
+
+	describe("keyOf", function () {
+
+		it("should return the property of needle", function () {
+			var data = {
+				"first": {"name": "first"},
+				"second": {"name": "second"}
+			};
+
+			var key = o.keyOf(data, data.second);
+
+			expect(key).to.eql("second");
+		});
+
+		it("should return null of not found", function () {
+			var data = {
+				"first": {"name": "first"},
+				"second": {"name": "second"}
+			};
+
+			var key = o.keyOf(data, {});
+
+			expect(key).to.be.null;
+		});
+	});
+});
