84a1edde642111ad7db514844edbac519648f8a3
Sascha Goldhofer <post@saschagoldhofer.de>
2015-02-01 16:36:32 +0100

    Introduce pointer.join

---
+++ test/unit/pointer/join.test.js
@@ -0,0 +1,29 @@
+var expect = require("chai").expect;
+
+var pointer = require("../../../lib/pointer"),
+	join = pointer.join;
+
+
+describe.only("pointer.join()", function () {
+
+	it("should return a json pointer", function () {
+
+		var pointer = join();
+
+		expect(pointer).to.eq("#/");
+	});
+
+	it("should join multiple pointers", function () {
+
+		var pointer = join("#/parent", "#/child", "#/target");
+
+		expect(pointer).to.eq("#/parent/child/target");
+	});
+
+	it("should join single words to a pointer", function () {
+
+		var pointer = join("parent", "child", "target");
+
+		expect(pointer).to.eq("#/parent/child/target");
+	});
+});
