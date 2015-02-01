c28533cffb8c43d3a97bb83e2bb7afd0ce65987e
Sascha Goldhofer <post@saschagoldhofer.de>
2015-02-01 16:28:53 +0100

    Add update has_many relationships

--- test/unit/relation/update.test.js
+++ test/unit/relation/update.test.js
@@ -29,8 +29,8 @@ describe("relation.update", function () {
 	describe("has_one", function () {
 
 		it("should update relationships in pivot", function () {
-			var relationship = "articles has_one:contents as:article through:articles_contents";
-			relationship = {
+			// "articles has_one:contents as:article through:articles_contents";
+			var relationship = {
 				model: "#/articles",
 				type: "has_one",
 				references: "#/contents",
@@ -46,8 +46,8 @@ describe("relation.update", function () {
 		});
 
 		it("should update reference model and pivot table", function () {
-			var relationship = "articles hasOne:contents as:article through:articles_contents";
-			relationship = {
+			// "articles hasOne:contents as:article through:articles_contents";
+			var relationship = {
 				model: "#/articles",
 				type: "has_one",
 				references: "#/contents",
@@ -65,8 +65,8 @@ describe("relation.update", function () {
 		});
 
 		it("should update reference model", function () {
-			var relationship = "articles hasOne:contents in:article";
-			relationship = {
+			// "articles hasOne:contents as:article";
+			var relationship = {
 				model: "#/articles",
 				type: "has_one",
 				references: "#/contents",
@@ -79,6 +79,90 @@ describe("relation.update", function () {
 			update(data, relationship);
 
 			expect(data["contents"].latest).to.deep.eql(data["articles"].latest["article"]);
+		});
+	});
+
+	describe("has_many", function () {
+
+		beforeEach(function () {
+			data = {
+
+				"articles": {
+					"latest": {
+						"title": "Each article has one content"
+						// articles: [ {}, {} ]
+					}
+				},
+
+				"contents": {
+					"latest_content": {
+						"body": "latest content"
+					},
+					"content": {
+						"body": "another content"
+					}
+				},
+
+				"articles_contents": {}
+			};
+		});
+
+		it("should update relationships in pivot", function () {
+			// "articles has_many:contents as:articles through:articles_contents";
+			var relationship = {
+				model: "#/articles",
+				type: "has_many",
+				references: "#/contents",
+				alias: "articles",
+				through: "articles_contents"
+			};
+			// link latest content to article
+			data.articles.latest.articles = [ data.contents.latest_content, data.contents.content ];
+
+			update(data, relationship);
+
+			expect(data["articles_contents"]["latest"].length).to.eql(2);
+			expect(data["articles_contents"]["latest"].indexOf("latest_content")).to.not.eql(-1);
+			expect(data["articles_contents"]["latest"].indexOf("content")).to.not.eql(-1);
+		});
+
+		it("should update relationships in referenced model", function () {
+			// "articles has_many:contents as:articles through:articles_contents";
+			var relationship = {
+				model: "#/articles",
+				type: "has_many",
+				references: "#/contents",
+				alias: "articles",
+				through: "articles_contents"
+			};
+			// link latest content to article
+			data.articles.latest.articles = [ data.contents.latest_content, data.contents.content ];
+			delete data.contents.latest_content;
+			delete data.contents.content;
+
+			update(data, relationship);
+
+			expect(data.contents["latest"]).to.eql(data.articles.latest.articles[0]);
+			expect(data.contents["latest-0"]).to.eql(data.articles.latest.articles[1]);
+		});
+
+		it("should update foreign_keys", function () {
+			// "articles has_many:contents as:articles through:articles_contents";
+			var relationship = {
+				model: "#/articles",
+				type: "has_many",
+				references: "#/contents",
+				foreign_key: "articles_pk",
+				alias: "articles"
+			};
+			// link latest content to article
+			data.articles.latest.articles = [ data.contents.latest_content, data.contents.content ];
+
+			update(data, relationship);
+
+			expect(data.articles.latest.articles_pk.length).to.eql(2);
+			expect(data.articles.latest.articles_pk.indexOf("latest_content")).to.not.eql(-1);
+			expect(data.articles.latest.articles_pk.indexOf("content")).to.not.eql(-1);
 		});
 	});
 });
