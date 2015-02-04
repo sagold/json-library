55d07c7bce6ec44d5aeebaa493e961a79cca6a17
Sascha Goldhofer <post@saschagoldhofer.de>
2015-02-04 08:47:13 +0100

    Add build & dist tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,5 @@
-var Dot = require("mocha/lib/reporters/dot"),
+var webpack = require("webpack"),
+    Dot = require("mocha/lib/reporters/dot"),
     sys = require('sys'),
     exec = require('child_process').exec;
 
@@ -29,9 +30,9 @@ module.exports = function (grunt) {
 
     grunt.initConfig({
 
-        mochaTest: {
+        "mochaTest": {
 
-            test: {
+            "test": {
                 src: ['test/**/*.test.js'],
                 options: {
                     reporter: withStatusIcon(Dot),
@@ -40,24 +41,102 @@ module.exports = function (grunt) {
             }
         },
 
-        watch: {
+        "watch": {
 
-            scripts: {
-
+            "scripts": {
                 files: ['test/**/*.test.js', 'lib/**/*.js'],
                 tasks: ['mochaTest'],
                 options: {
-
                     spawn: true
                 }
+            }
+        },
+
+        "webpack": {
+
+            "build": {
+                // webpack options
+                entry: "./lib/index.js",
+                output: {
+                    path: "build/",
+                    filename: "JsonLibrary.js",
+                    // libraryTarget: "var",
+                    library: "JsonLibrary"
+                },
+
+                stats: {
+                    // Configure the console output
+                    colors: false,
+                    modules: true,
+                    reasons: true
+                },
+                // stats: false disables the stats output
+                storeStatsTo: "xyz", // writes the status to a variable named xyz
+                // you may use it later in grunt i.e. <%= xyz.hash %>
+
+                progress: false, // Don't show progress
+                // Defaults to true
+                failOnError: false, // don't report error to grunt if webpack find errors
+                // Use this if webpack errors are tolerable and grunt should continue
+                watch: false, // use webpacks watcher
+                // You need to keep the grunt process alive
+                keepalive: false, // don't finish the grunt task
+            },
+
+            "dist": {
+                // webpack options
+                entry: "./lib/index.js",
+                output: {
+                    path: "dist/",
+                    filename: "JsonLibrary.js",
+                    // libraryTarget: "var",
+                    library: "JsonLibrary"
+                },
+
+                stats: {
+                    // Configure the console output
+                    colors: false,
+                    modules: true,
+                    reasons: true
+                },
+                // stats: false disables the stats output
+                storeStatsTo: "xyz", // writes the status to a variable named xyz
+                // you may use it later in grunt i.e. <%= xyz.hash %>
+
+                progress: false, // Don't show progress
+                // Defaults to true
+                failOnError: false, // don't report error to grunt if webpack find errors
+                // Use this if webpack errors are tolerable and grunt should continue
+                watch: false, // use webpacks watcher
+                // You need to keep the grunt process alive
+                keepalive: false, // don't finish the grunt task
+                // module: {
+                //     loaders: [
+                //         {
+                //             test: /.*\.js$/, loader: "uglify"
+                //         }
+                //     ]
+                // }
+                plugins: [
+                    new webpack.optimize.UglifyJsPlugin({
+                        compress: {
+                            drop_console: true
+                        }
+                    })
+                ]
             }
         }
     });
 
     grunt.loadNpmTasks('grunt-mocha-test');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-webpack');
     // grunt.loadNpmTasks('grunt-notify');
 
+    // testing
     grunt.registerTask('test', "mochaTest");
     grunt.registerTask('tdd', 'watch');
+    // build
+    grunt.registerTask('build', 'webpack:build');
+    grunt.registerTask('dist', 'webpack:dist');
 };
