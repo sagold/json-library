5d081bad3ec745be0d5343108332cc8736018168
Sascha Goldhofer <post@saschagoldhofer.de>
2015-02-09 09:48:59 +0100

    Extend pointer.set to support arrays

--- test/unit/pointer/set.test.js
+++ test/unit/pointer/set.test.js
@@ -18,12 +18,49 @@ describe("pointer.set", function () {
 		expect(result.path.to.property).to.be.true
 	});
 
-	it("should add not remove any other properties", function () {
+	it("should not remove any other properties", function () {
 		var result = pointer.set({
 			"path": { "to": { "id": "parent"} }
 		}, "#/path/to/property", true);
 
 		expect(result.path.to.property).to.be.true
 		expect(result.path.to.id).to.eq("parent");
+	});
+
+	it("should insert array for []", function () {
+		var result = pointer.set({}, "#/array/[]", true);
+
+		expect(result.array).to.be.an.array;
+		expect(result.array.length).to.eq(1);
+	});
+
+	it("should insert index in array", function () {
+		var result = pointer.set({}, "#/array/[1]", true);
+
+		expect(result.array).to.be.an.array;
+		expect(result.array.length).to.eq(2);
+		expect(result.array[1]).to.be.true;
+	});
+
+	it("should insert array in array", function () {
+		var result = pointer.set({}, "#/array/[]/[]", true);
+
+		expect(result.array[0]).to.be.an.array;
+		expect(result.array[0][0]).to.be.true;
+	});
+
+	it("should insert array to index in array", function () {
+		var result = pointer.set({}, "#/array/[1]/[]", true);
+
+		expect(result.array[1]).to.be.an.array;
+		expect(result.array[1][0]).to.be.true;
+	});
+
+	it("should insert object in array", function () {
+		var result = pointer.set({}, "#/array/[1]/valid", true);
+
+		expect(result.array.length).to.eq(2);
+		expect(result.array[1]).to.be.an.object;
+		expect(result.array[1].valid).to.be.true;
 	});
 });
