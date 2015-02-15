fcd1320bdeeb3f08a7f7b7f44c14e6b22941374d
Sascha Goldhofer <post@saschagoldhofer.de>
2015-02-15 18:36:05 +0100

    Refactor simplify relation.load tests

--- test/unit/relation/load.test.js
+++ test/unit/relation/load.test.js
@@ -4,9 +4,9 @@ var load = require("../../../lib/relation/load");
 
 describe("relation.load", function () {
 
-	describe("has_one", function () {
+	var data;
 
-		var data;
+	describe("has_one", function () {
 
 		beforeEach(function () {
 			data = {
@@ -32,28 +32,58 @@ describe("relation.load", function () {
 			}
 		});
 
-		it("should resolve to foreign_key", function () {
-			var model = load(data, "model has_one:relatedModel on:related_pk");
+		describe("foreign key", function () {
 
-			expect(model.first.related_pk).to.eql(data.relatedModel.first);
+			it("should resolve on foreign_key", function () {
+				var model = load(data, "model has_one:relatedModel on:related_pk");
+
+				expect(model.first.related_pk).to.eql(data.relatedModel.first);
+			});
+
+			it("should ignore already linked model", function () {
+				var model, relation = "model has_one:relatedModel on:related_pk";
+				model = load(data, relation);
+
+				expect(model.first.related_pk).to.eql(data.relatedModel.first);
+			});
+
+			it("should resolve to alias", function () {
+				var model = load(data, "model has_one:relatedModel as:relationship on:related_pk");
+
+				expect(model.first.relationship).to.eql(data.relatedModel.first);
+			});
+
+			it("should replace model on alias", function () {
+				var model, relation = "model has_one:relatedModel as:relationship on:related_pk";
+				model = load(data, relation);
+				model.first.related_pk = "second";
+				model = load(data, relation);
+
+				expect(model.first.relationship).to.eql(data.relatedModel.second);
+			});
 		});
 
-		it("should resolve to alias", function () {
-			var model = load(data, "model has_one:relatedModel as:relationship on:related_pk");
+		describe("pivot table", function () {
 
-			expect(model.first.relationship).to.eql(data.relatedModel.first);
-		});
+			it("should resolve on alias", function () {
+				var model = load(data, "model has_one:relatedModel as:relationship through:model_relatedModel");
 
-		it("should resolve by pivot table", function () {
-			var model = load(data, "model has_one:relatedModel as:relationship through:model_relatedModel");
+				expect(model.first.relationship).to.eql(data.relatedModel.first);
+			});
 
-			expect(model.first.relationship).to.eql(data.relatedModel.first);
+			it ("should replace current link of pivot on alias", function () {
+				var model, relation = "model has_one:relatedModel as:relationship through:model_relatedModel"
+				load(data, relation);
+				data.model_relatedModel.first = "second";
+
+				model = load(data, relation);
+
+				expect(model.first.relationship).to.eql(data.relatedModel.second);
+			});
 		});
 	});
 
 	describe("has_many", function () {
-
-		var data;
 
 		beforeEach(function () {
 			data = {
@@ -79,19 +109,54 @@ describe("relation.load", function () {
 			}
 		});
 
-		it("should load object within foreign_key array", function () {
-			var model = load(data, "model has_many:relatedModel on:rel");
+		describe("foreign key", function () {
 
-			expect(model.first.rel.length).to.eql(2);
-			expect(model.first.rel[1]).to.eql(data.relatedModel.third);
+			it("should replace keys by related objects", function () {
+				var model = load(data, "model has_many:relatedModel on:rel");
+
+				expect(model.first.rel.length).to.eql(2);
+				expect(model.first.rel[1]).to.eql(data.relatedModel.third);
+			});
+
+			it("should add related objects to alias", function () {
+				var model = load(data, "model has_many:relatedModel as:alias on:rel");
+
+				expect(model.first.alias.length).to.eql(2);
+				expect(model.first.alias[1]).to.eql(data.relatedModel.third);
+			});
+
+			it("should replace related objects of alias", function () {
+				var relation = "model has_many:relatedModel as:alias on:rel";
+				model = load(data, relation);
+				model.first.rel = ["first", "second"];
+				model = load(data, relation);
+
+				expect(model.first.alias.length).to.eql(2);
+				expect(model.first.alias[0]).to.eql(data.relatedModel.first);
+				expect(model.first.alias[1]).to.eql(data.relatedModel.second);
+			});
 		});
 
-		it ("should resolve array through pivot table", function () {
-			var model = load(data, "model has_many:relatedModel as:rel through:model_relatedModel");
+		describe("pivot table", function () {
 
-			expect(model.first.rel.length).to.eql(2);
-			expect(model.second.rel.length).to.eql(3);
-			expect(model.first.rel[1]).to.eql(data.relatedModel.third);
+			it ("should add related objects to alias", function () {
+				var model = load(data, "model has_many:relatedModel as:links through:model_relatedModel");
+
+				expect(model.first.links.length).to.eql(2);
+				expect(model.second.links.length).to.eql(3);
+				expect(model.first.links[1]).to.eql(data.relatedModel.third);
+			});
+
+			it ("should replace related objects on alias", function () {
+				var model, relation = "model has_many:relatedModel as:links through:model_relatedModel"
+				load(data, relation);
+				data.model_relatedModel.first = ["second"];
+
+				model = load(data, relation);
+
+				expect(model["first"].links.length).to.eq(1);
+				expect(model["first"].links[0]).to.eq(data.relatedModel.second);
+			});
 		});
 	});
 });
