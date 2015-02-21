d265193c5c37943cf99ca5e3cf089ef821d5a24b
Sascha Goldhofer <post@saschagoldhofer.de>
2015-02-21 12:47:28 +0100

    Add update relationship tests

--- test/unit/relation/RelationshipFactory.test.js
+++ test/unit/relation/RelationshipFactory.test.js
@@ -8,11 +8,13 @@ var o = require("../../../lib/object"),
 
 describe("RelationFactory", function () {
 
-	var data;
+	var data, relation;
+
 
 	describe("has_one:foreign_key", function () {
 
 		beforeEach(function () {
+			// create new data object
 			data = {
 				"person": {
 					"alfred": {
@@ -25,43 +27,51 @@ describe("RelationFactory", function () {
 					"big": {"id": "big"}
 				}
 			};
+			// and setup relationship
+			relation = new Relation(data, "person has_one:nose on:face");
 		});
 
 		it("should return type has_one:foreign_key", function () {
-			var relation = new Relation(data, "person has_one:nose on:face");
-
 			expect(relation.id).to.eq("has_one:foreign_key");
 		});
 
 		// load
 
 		it("should add linked tupel to parent tupel", function () {
-			var relation = new Relation(data, "person has_one:nose on:face");
 			relation.load("alfred");
 
 			expect(data.person.alfred.face.id).to.eq("large");
 		});
 
-		it("should not replace loaded tupel", function () {
-			var relation = new Relation(data, "person has_one:nose on:face");
+		it("should replace loaded tupel", function () {
 			relation.load("alfred");
+			data.person.alfred.face = "big";
 			relation.load("alfred");
 
-			expect(data.person.alfred.face.id).to.eq("large");
+			expect(data.person.alfred.face.id).to.eq("big");
+		});
+
+		// update
+
+		it("should add new tupel to relatedModel", function () {
+			var id;
+			data.person.alfred.face = {"id": "broad"};
+			relation.update("alfred");
+			id = o.keyOf(data.nose, data.person.alfred.face);
+
+			expect(id).to.not.be.null;
+			expect(data.nose[id].id).to.eq("broad");
 		});
 
 		// unload
 
 		it("should reverse load", function () {
 			var orig = o.copy(data);
-			var relation = new Relation(data, "person has_one:nose on:face");
 			relation.load("alfred");
 			relation.unload("alfred");
 
 			expect(orig.person.alfred).to.deep.eq(data.person.alfred);
 		});
-
-		// update
 
 		// add
 		// remove
@@ -74,6 +84,7 @@ describe("RelationFactory", function () {
 	describe("has_one:foreign_key:alias", function () {
 
 		beforeEach(function () {
+			// create new data object
 			data = {
 				"person": {
 					"alfred": {
@@ -86,36 +97,63 @@ describe("RelationFactory", function () {
 					"big": {"id": "big"}
 				}
 			};
+			// create relationship
+			relation = relation = new Relation(data, "person has_one:nose on:face as:nose");
 		});
 
 		it("should return type has_one:foreign_key:alias", function () {
-			var relation = new Relation(data, "person has_one:nose on:face as:nose");
-
 			expect(relation.id).to.eq("has_one:foreign_key:alias");
 		});
 
 		// load
 
 		it("should add linked tupel in foreign key to parent tupel", function () {
-			var relation = new Relation(data, "person has_one:nose on:face as:nose");
 			relation.load("alfred");
 
 			expect(data.person.alfred.nose.id).to.eq("large");
 		});
 
 		it("should keep linked tupel intact", function () {
-			var relation = new Relation(data, "person has_one:nose on:face as:nose");
 			relation.load("alfred");
 			relation.load("alfred");
 
 			expect(data.person.alfred.nose.id).to.eq("large");
 		});
 
+		it("should replace loaded tupel", function () {
+			relation.load("alfred");
+			data.person.alfred.face = "big";
+			relation.load("alfred");
+
+			expect(data.person.alfred.nose.id).to.eq("big");
+		});
+
+		// update
+
+		it("should add new tupel to related model", function () {
+			var id;
+			data.person.alfred.nose = {"id": "broad"};
+			relation.update("alfred");
+			id = o.keyOf(data.nose, data.person.alfred.nose);
+
+			expect(id).to.not.be.null;
+			expect(data.nose[id].id).to.eq("broad");
+		});
+
+		it("should update foreign key", function () {
+			var id;
+			data.person.alfred.nose = {"id": "broad"};
+			relation.update("alfred");
+			id = o.keyOf(data.nose, data.person.alfred.nose);
+
+			expect(id).to.not.be.null;
+			expect(data.person.alfred.face).to.eq(id);
+		});
+
 		// unload
 
 		it("should reverse load", function () {
 			var orig = o.copy(data);
-			var relation = new Relation(data, "person has_one:nose on:face as:nose");
 			relation.load("alfred");
 			relation.unload("alfred");
 
@@ -123,7 +161,6 @@ describe("RelationFactory", function () {
 		});
 
 		it("should update foreign key on unload", function () {
-			var relation = new Relation(data, "person has_one:nose on:face as:nose");
 			data.person.alfred.nose = data.nose.big;
 			relation.unload("alfred");
 
@@ -142,6 +179,7 @@ describe("RelationFactory", function () {
 	describe("has_one:through:alias", function () {
 
 		beforeEach(function () {
+			// create new data object
 			data = {
 				"person": {
 					"alfred": {
@@ -160,29 +198,57 @@ describe("RelationFactory", function () {
 					"alfred": "large"
 				}
 			};
+			// create relationship
+			relation = new Relation(data, "person has_one:nose through:person_nose as:nose");
 		});
 
 		it("should return type has_one:through:alias", function () {
-			var relation = new Relation(data, "person has_one:nose through:person_nose as:nose");
-
 			expect(relation.id).to.eq("has_one:through:alias");
 		});
 
 		// load
 
 		it("should add linked tupel in pivot to parent tupel", function () {
-			var relation = new Relation(data, "person has_one:nose through:person_nose as:nose");
 			relation.load("alfred");
 
 			expect(data.person.alfred.nose.id).to.eq("large");
 		});
 
 		it("should keep linked tupel intact", function () {
-			var relation = new Relation(data, "person has_one:nose through:person_nose as:nose");
 			relation.load("alfred");
 			relation.load("alfred");
 
 			expect(data.person.alfred.nose.id).to.eq("large");
+		});
+
+		it("should replace loaded tupel", function () {
+			relation.load("alfred");
+			data.person_nose.alfred = "big";
+			relation.load("alfred");
+
+			expect(data.person.alfred.nose.id).to.eq("big");
+		});
+
+		// update
+
+		it("should add new tupel to related model", function () {
+			var id, tupel = {"id": "broad"};
+			data.person.alfred.nose = tupel;
+			relation.update("alfred");
+			id = o.keyOf(data.nose, tupel);
+
+			expect(id).to.not.be.null;
+			expect(data.nose[id].id).to.eq("broad");
+		});
+
+		it("should update pivot", function () {
+			var id, tupel = {"id": "broad"};
+			data.person.alfred.nose = tupel;
+			relation.update("alfred");
+			id = o.keyOf(data.nose, tupel);
+
+			expect(id).to.not.be.null;
+			expect(data.person_nose.alfred).to.eq(id);
 		});
 
 		// unload
@@ -203,8 +269,6 @@ describe("RelationFactory", function () {
 
 			expect(data.person_nose.alfred).to.eq("big");
 		});
-
-		// update
 
 		// add
 		// remove
@@ -235,18 +299,17 @@ describe("RelationFactory", function () {
 					}
 				}
 			};
+			// create relationship
+			relation = new Relation(data, "person has_many:ears on:ears");
 		});
 
 		it("should return type has_many:foreign_key", function () {
-			var relation = new Relation(data, "person has_many:ears on:ears");
-
 			expect(relation.id).to.eq("has_many:foreign_key");
 		});
 
 		// load
 
 		it("should replace all keys at foreign_key by related tupels", function () {
-			var relation = new Relation(data, "person has_many:ears on:ears");
 			relation.load("alfred");
 
 			expect(data.person.alfred.ears.length).to.eq(2);
@@ -254,12 +317,41 @@ describe("RelationFactory", function () {
 		});
 
 		it("should keep linked tupels intact", function () {
-			var relation = new Relation(data, "person has_many:ears on:ears");
 			relation.load("alfred");
 			relation.load("alfred");
 
 			expect(data.person.alfred.ears.length).to.eq(2);
 			expect(data.person.alfred.ears[1].id).to.eq("big");
+		});
+
+		it("should update linked tupels", function () {
+			relation.load("alfred");
+			data.person.alfred.ears[0] = "broad";
+			relation.load("alfred");
+
+			expect(data.person.alfred.ears.length).to.eq(2);
+			expect(data.person.alfred.ears[0].id).to.eq("broad");
+		});
+
+		// update
+
+		it("should add new tupel to related model", function () {
+			var id, tupel = {"id": "square"};
+			data.person.alfred.ears[0] = tupel;
+			relation.update("alfred");
+			id = o.keyOf(data.ears, tupel);
+
+			expect(id).to.not.be.null;
+			expect(data.ears[id].id).to.eq("square");
+		});
+
+		it("should not modify foreign keys", function () {
+			var id, tupel = {"id": "square"};
+			data.person.alfred.ears[0] = tupel;
+			relation.update("alfred");
+
+			expect(data.person.alfred.ears[0]).to.eq(tupel);
+			expect(data.person.alfred.ears[1]).to.eq("big");
 		});
 
 		// unload
@@ -282,8 +374,6 @@ describe("RelationFactory", function () {
 			expect(data.person.alfred.ears[0]).to.eq("broad");
 			expect(data.person.alfred.ears[1]).to.eq("big");
 		});
-
-		// update
 
 		// add
 		// remove
@@ -314,18 +404,17 @@ describe("RelationFactory", function () {
 					}
 				}
 			};
+			// create relationship
+			relation = new Relation(data, "person has_many:ears as:feet on:ears");
 		});
 
 		it("should return type has_many:foreign_key:alias", function () {
-			var relation = new Relation(data, "person has_many:ears as:feet on:ears");
-
 			expect(relation.id).to.eq("has_many:foreign_key:alias");
 		});
 
 		// load
 
 		it("should link all tupels specified in foreign_key on alias", function () {
-			var relation = new Relation(data, "person has_many:ears as:feet on:ears");
 			relation.load("alfred");
 
 			expect(data.person.alfred.feet.length).to.eq(2);
@@ -333,7 +422,6 @@ describe("RelationFactory", function () {
 		});
 
 		it("should keep linked tupels intact", function () {
-			var relation = new Relation(data, "person has_many:ears as:feet on:ears");
 			relation.load("alfred");
 			relation.load("alfred");
 
@@ -341,11 +429,42 @@ describe("RelationFactory", function () {
 			expect(data.person.alfred.feet[1].id).to.eq("big");
 		});
 
+		it("should load new tupel", function () {
+			relation.load("alfred");
+			data.person.alfred.ears[2] = "broad";
+			relation.load("alfred");
+
+			expect(data.person.alfred.feet.length).to.eq(3);
+			expect(data.person.alfred.feet[2].id).to.eq("broad");
+		});
+
+		// update
+
+		it("should add new tupel to related model", function () {
+			var id, tupel = {"id": "square"};
+			data.person.alfred.feet = [tupel];
+			relation.update("alfred");
+			id = o.keyOf(data.ears, tupel);
+
+			expect(id).to.not.be.null;
+			expect(data.ears[id].id).to.eq("square");
+		});
+
+		it("should update foreign keys", function () {
+			var id, tupel = {"id": "square"};
+			data.person.alfred.feet = [tupel];
+			relation.update("alfred");
+			id = o.keyOf(data.ears, tupel);
+
+			expect(id).to.not.be.null;
+			expect(data.person.alfred.ears.length).to.eq(1);
+			expect(data.person.alfred.ears[0]).to.eq(id);
+		});
+
 		// unload
 
 		it("should reverse load", function () {
 			var orig = o.copy(data);
-			var relation = new Relation(data, "person has_many:ears as:feet on:ears");
 			relation.load("alfred");
 			relation.unload("alfred");
 
@@ -353,7 +472,7 @@ describe("RelationFactory", function () {
 		});
 
 		it("should update foreign keys on unload", function () {
-			var relation = new Relation(data, "person has_many:ears as:feet on:ears");
+
 			data.person.alfred.feet = [data.ears.broad, data.ears.big];
 			relation.unload("alfred");
 
@@ -362,9 +481,7 @@ describe("RelationFactory", function () {
 			expect(data.person.alfred.ears[1]).to.eq("big");
 		});
 
-		// update
-
-		// add
+		// add "person has_many:ears as:feet on:ears"
 		// remove
 		// insert
 		// delete
@@ -386,24 +503,26 @@ describe("RelationFactory", function () {
 					},
 					"large": {
 						"id": "large"
+					},
+					"square": {
+						"id": "square"
 					}
 				},
 				"person_ears": {
 					"alfred": ["large", "big"]
 				}
 			};
+			// create relationship
+			relation = new Relation(data, "person has_many:ears as:ears through:person_ears");
 		});
 
 		it("should return type has_many:through:alias", function () {
-			var relation = new Relation(data, "person has_many:ears as:ears through:person_ears");
-
 			expect(relation.id).to.eq("has_many:through:alias");
 		});
 
 		// load
 
 		it("should link all tupels speicified in pivot on alias", function () {
-			var relation = new Relation(data, "person has_many:ears as:ears through:person_ears");
 			relation.load("alfred");
 
 			expect(data.person.alfred.ears.length).to.eq(2);
@@ -411,7 +530,6 @@ describe("RelationFactory", function () {
 		});
 
 		it("should keep linked tupels intact", function () {
-			var relation = new Relation(data, "person has_many:ears as:ears through:person_ears");
 			relation.load("alfred");
 			relation.load("alfred");
 
@@ -419,11 +537,42 @@ describe("RelationFactory", function () {
 			expect(data.person.alfred.ears[1].id).to.eq("big");
 		});
 
+		it("should replace new tupel", function () {
+			relation.load("alfred");
+			data.person_ears.alfred = ["square"];
+			relation.load("alfred");
+
+			expect(data.person.alfred.ears.length).to.eq(1);
+			expect(data.person.alfred.ears[0].id).to.eq("square");
+		});
+
+		// update
+
+		it("should add new tupel to related model", function () {
+			var id, tupel = {"id": "round"};
+			data.person.alfred.ears = [tupel];
+			relation.update("alfred");
+			id = o.keyOf(data.ears, tupel);
+
+			expect(id).to.not.be.null;
+			expect(data.ears[id].id).to.eq("round");
+		});
+
+		it("should update pivot", function () {
+			var id, tupel = {"id": "round"};
+			data.person.alfred.ears = [tupel];
+			relation.update("alfred");
+			id = o.keyOf(data.ears, tupel);
+
+			expect(id).to.not.be.null;
+			expect(data.person_ears.alfred.length).to.eq(1);
+			expect(data.person_ears.alfred[0]).to.eq(id);
+		});
+
 		// unload
 
 		it("should reverse load", function () {
 			var orig = o.copy(data);
-			var relation = new Relation(data, "person has_many:ears as:ears through:person_ears");
 			relation.load("alfred");
 			relation.unload("alfred");
 
@@ -431,7 +580,6 @@ describe("RelationFactory", function () {
 		});
 
 		it("should update pivot table on unload", function () {
-			var relation = new Relation(data, "person has_many:ears as:ears through:person_ears");
 			data.person.alfred.ears = [data.ears.big];
 			relation.unload("alfred");
 
@@ -439,7 +587,7 @@ describe("RelationFactory", function () {
 			expect(data.person_ears.alfred[0]).to.eq("big");
 		});
 
-		// update
+		// update "person has_many:ears as:ears through:person_ears"
 
 		// add
 		// remove
