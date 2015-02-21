d5e8246a8a41c37c5be39ebdcf18b32a1988656c
Sascha Goldhofer <post@saschagoldhofer.de>
2015-02-21 14:27:51 +0100

    Cleanup

--- test/integration/relation.test.js
+++ test/integration/relation.test.js
@@ -2,7 +2,7 @@ var expect = require("chai").expect,
 	path = require("path"),
 	fs = require("fs");
 
-var relation = require("../../lib/relation");
+var Relationship = require("../../lib/relation").Relationship;
 
 describe("relation", function () {
 
@@ -16,38 +16,35 @@ describe("relation", function () {
 	});
 
 	it("should unload has_one:foreign_key to original object", function () {
-		var model, shirt_size = "shirt has_one:size on:related_size";
-		model = relation.load(modifiedData, shirt_size);
+		var relation = new Relationship(modifiedData, "shirt has_one:size on:related_size");
+		relation.loadAll();
 		// log
 		fs.writeFileSync(path.join(__dirname, "../log/simple.has_one.loaded.json"), JSON.stringify(modifiedData, null, 4));
-
 		// relationship should have been loaded
-		expect(model.hotpink.related_size.name).to.eq("small");
+		expect(modifiedData.shirt.hotpink.related_size.name).to.eq("small");
 
-		model = relation.unload(modifiedData, shirt_size);
+		relation.unloadAll();
 		// log
 		fs.writeFileSync(path.join(__dirname, "../log/simple.has_one.unloaded.json"), JSON.stringify(modifiedData, null, 4));
 
-		expect(model.hotpink.related_size).to.eq("small");
+		expect(modifiedData.shirt.hotpink.related_size).to.eq("small");
 		expect(originalData).to.deep.eql(modifiedData);
 	});
 
 	it ("should unload has_many:through:alias to original object", function () {
-		var model, shirt_sizes = "shirt has_many:size as:sizes through:shirt_sizes";
-		model = relation.load(modifiedData, shirt_sizes);
+		var relation = new Relationship(modifiedData, "shirt has_many:size as:sizes through:shirt_sizes");
+		relation.loadAll();
 		// log
 		fs.writeFileSync(path.join(__dirname, "../log/simple.has_many.loaded.json"), JSON.stringify(modifiedData, null, 4));
 		// relationship should have been loaded
-		expect(model.white.sizes.length).to.eq(2);
-		expect(model.white.sizes[1].name).to.eq("small");
+		expect(modifiedData.shirt.white.sizes.length).to.eq(2);
+		expect(modifiedData.shirt.white.sizes[1].name).to.eq("small");
 
-		model = relation.unload(modifiedData, shirt_sizes);
+		relation.unloadAll();
 		// log
 		fs.writeFileSync(path.join(__dirname, "../log/simple.has_many.unloaded.json"), JSON.stringify(modifiedData, null, 4));
 
-		expect(model.white.sizes).to.be.undefined;
+		expect(modifiedData.shirt.white.sizes).to.be.undefined;
 		expect(originalData).to.deep.eql(modifiedData);
-
 	});
-
 });
