38c53f4792ccdef4c57811d6afbe6f0f4cd79f09
Sascha Goldhofer <post@saschagoldhofer.de>
2016-11-15 20:20:39 +0100

    Fix package imports

--- pointer.js
+++ pointer.js
@@ -1,4 +1,4 @@
 "use strict";
 
 
-module.exports = require("@sagold/json-pointer");
+module.exports = require("gson-pointer");
--- query.js
+++ query.js
@@ -1,4 +1,4 @@
 "use strict";
 
 
-module.exports = require("@sagold/json-query");
+module.exports = require("gson-query");
